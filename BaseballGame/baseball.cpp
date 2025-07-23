36110bbbd064cf860354aaf1e013cd0eef45b58c
ehju <eh13.ju@samsung.com>
2025-07-23 13:16:14 +0900

    [refactoring] TDD step 3

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -5,19 +5,28 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		assertIlligalArgument(guessNumber);
+
+	}
+private:
+	bool isDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[1] == guessNumber[2]
+			|| guessNumber[0] == guessNumber[2];
+	}
+	void assertIlligalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters. ");
 		}
-		for (char c :guessNumber) {
-			if (c < '0' || c >'9') {
+		for (char c : guessNumber) {
+			if (c >= '0' && c <= '9') {
 				throw invalid_argument("Must be number");
 			}
 		}
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[1] == guessNumber[2]
-			|| guessNumber[0] == guessNumber[2]) {
+		if (isDuplicatedNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number");
 		}
-
 	}
 };
