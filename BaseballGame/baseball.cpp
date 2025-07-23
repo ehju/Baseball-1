4f52a0feeb488481c30357542c81e1c6726f5fa2
ehju <eh13.ju@samsung.com>
2025-07-23 13:28:28 +0900

    [feature] TDD step 4

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -2,28 +2,35 @@
 
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+	explicit Baseball(const string& question) : question(question) {}
+	GuessResult guess(const string& guessNumber) {
 		assertIlligalArgument(guessNumber);
-
+		return { true,3,0 };
 	}
 private:
-	bool isDuplicatedNumber(const std::string& guessNumber)
+	string question;
+	bool isDuplicatedNumber(const string& guessNumber)
 	{
 		return guessNumber[0] == guessNumber[1]
 			|| guessNumber[1] == guessNumber[2]
 			|| guessNumber[0] == guessNumber[2];
 	}
-	void assertIlligalArgument(const std::string& guessNumber)
+	void assertIlligalArgument(const string& guessNumber)
 	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters. ");
 		}
-		for (char c : guessNumber) {
-			if (c >= '0' && c <= '9') {
-				throw invalid_argument("Must be number");
-			}
+		for (char ch : guessNumber) {
+			if (ch >= '0' && ch <= '9')  continue;
+			throw invalid_argument("Must be number");
 		}
 		if (isDuplicatedNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number");
--- BaseballGame/baseball_test.cpp
+++ BaseballGame/baseball_test.cpp
@@ -3,7 +3,7 @@
 
 class BaseballFixture : public testing::Test {
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIlligalArgument(string guessNumber) {
 		try {
 			game.guess(guessNumber);
@@ -19,6 +19,13 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIlligalArgument("12");
 	assertIlligalArgument("12s");
 	assertIlligalArgument("121");
+}
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
 
 int main() {
