77452040b874ca08457fe4577681e6758ee85f8e
ehju <eh13.ju@samsung.com>
2025-07-23 12:55:50 +0900

    [feature] TDD step 1

--- BaseballGame/baseball_test.cpp
+++ BaseballGame/baseball_test.cpp
@@ -1,9 +1,12 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
 
 int main() {
 	::testing::InitGoogleMock();
